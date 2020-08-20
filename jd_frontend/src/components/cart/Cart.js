aa4b733f9d8ee1584eea217f5afd74cf37c8a858
980739252 <980739252@qq.com>
2020-08-20 22:38:15 +0800

    8-20

---
+++ jd_frontend/src/components/cart/Cart.js
@@ -0,0 +1,17 @@
+import React   from 'react';
+import './Cart.css';
+import  Header from './header/Header';
+import   Navigation from './navigation/Navigation'
+
+function Cart() {
+   return(
+       <div className="Cart">
+           <Header/>
+           <Navigation/>
+          
+          
+       </div>
+   );
+
+}
+export  default Cart;
